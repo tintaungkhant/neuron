e718f8c907558a67ca57432068bf1d5d7dc50b39
Tint Aung Khant <aherkersoemin@gmail.com>
2026-05-20 18:29:00 +0700

    feat(app): wire ProjectsModule + TelegramController + boot test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EngineModule } from './engine';
+import { ProjectsModule } from './projects/projects.module';
+import { TelegramController } from './triggers/telegram.controller';
 
 @Module({
-  imports: [EngineModule],
-  controllers: [AppController],
+  imports: [EngineModule, ProjectsModule],
+  controllers: [AppController, TelegramController],
   providers: [AppService],
 })
 export class AppModule {}
