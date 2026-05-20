d4cecdc18ae443ff422a1598046f90dfb262fd68
Tint Aung Khant <aherkersoemin@gmail.com>
2026-05-20 14:10:09 +0700

    feat(engine): public surface + wire EngineModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EngineModule } from './engine';
 
 @Module({
-  imports: [],
+  imports: [EngineModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/engine/index.ts
@@ -0,0 +1,7 @@
+export { Node } from './node';
+export { WorkflowEngine } from './engine';
+export { EngineModule } from './engine.module';
+export { WorkflowError } from './errors';
+export type { WorkflowFn } from './workflow';
+export type { Context } from './context';
+export type { Trace, TraceStep, SerializedError } from './trace';
